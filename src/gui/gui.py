6fec0ab89410ca1fcf0dd5c8181941159f9fe9cb
mhamaril <matti.hamarila@yahoo.com>
2020-11-27 11:16:57 +0200

    main_view.py toiminnallisuuksia lisätty

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -4,6 +4,7 @@ class GUI:
     def __init__(self, root):
         self.root = root
         self.current_view = None
+    
     def hide_current_view(self):
         if self.current_view:
             self.current_view.destroy()
@@ -12,9 +13,8 @@ class GUI:
 
     def show_matrix_view(self):
         self.hide_current_view()
-
-        
-        self.current_view = MainViev(self.root, "Matric Calculator")
+        self.current_view = MainViev(self.root, "Matric Calculator", 3)
+        self.current_view.initi
 
     def start(self):
         self.show_matrix_view()
